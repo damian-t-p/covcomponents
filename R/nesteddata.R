99590690d755349dc170e1e4ee69554c3e5eb9c2
Damian Pavlyshyn <damian.pavlyshyn@gmail.com>
2023-03-16 10:14:21 +1100

    Improve factor/level naming and add test cases

--- R/nesteddata.R
+++ R/nesteddata.R
@@ -1,59 +1,59 @@
 #' Create a `nesteddata` object from a data frame
 #'
-#' @param df Wide data frame with one column for each factor and one for each dimension of the
+#' @param df Wide data frame with one column for each level and one for each dimension of the
 #' response variable.
-#' @param levels Character vector containing the names of the factor variables. Must be supplied in
-#' the bottom-up order of nesting so that `level[i]` is nested inside `level[i+1]`.
+#' @param factors Character vector containing the names of the level variables. Must be supplied in
+#' the bottom-up order of nesting so that `factor[i]` is nested inside `factor[i+1]`.
 #'
 #' @return An object of class `nesteddata` containing the information necessary to compute covariance
 #' component estimates. This is usually smaller than `df`.
 #'
 #' @export
-nesteddata <- function(df, levels) {
+nesteddata <- function(df, factors) {
 
-  n_levels <- length(levels)
-  stopifnot(n_levels >= 2L)
+  n_factors <- length(factors)
+  stopifnot(n_factors >= 2L)
   
   value_matrix <- df %>%
-    dplyr::select(-all_of(levels)) %>%
+    dplyr::select(-all_of(factors)) %>%
     as.matrix()
 
   label_matrix <- df %>%
-    dplyr::transmute(across(all_of(levels), make.names)) %>%
+    dplyr::transmute(across(all_of(factors), make.names)) %>%
     as.matrix()
 
   parents    <- list()
   n_observed <- list()
   
-  for (i in 1L:(n_levels - 1L)) {
-    curr_level <- levels[i]
+  for (i in 1L:(n_factors - 1L)) {
+    curr_factor <- factors[i]
     
     curr_labels   <- label_matrix[, i]
     parent_labels <- label_matrix[, i + 1L]
 
     unique_curr_idx <- !duplicated(curr_labels)
 
-    n_observed[[curr_level]] <- tapply(curr_labels, parent_labels, \(v) length(unique(v)))
+    n_observed[[curr_factor]] <- c(tapply(curr_labels, parent_labels, \(v) length(unique(v))))
     
     if (i >= 2L) {
-      parents[[curr_level]] <- setNames(parent_labels[unique_curr_idx],
+      parents[[curr_factor]] <- setNames(parent_labels[unique_curr_idx],
                                        curr_labels[unique_curr_idx])
     }
   }
   
-  n_factors = c(
+  n_levels = c(
     sapply(n_observed, max),
-    length(unique(label_matrix[, n_levels]))
+    length(unique(label_matrix[, n_factors]))
   )
-  names(n_factors) <- levels
+  names(n_levels) <- factors
   
   new_nesteddata(
     sos_matrix = t(value_matrix) %*% value_matrix,
     group_sums = rowsum(value_matrix, group = label_matrix[, 2L]),
-    n_levels   = n_levels,
-    levels     = levels,
+    n_factors  = n_factors,
+    factors    = factors,
     parents    = parents,
-    n_factors  = n_factors, 
+    n_levels   = n_levels, 
     n_observed = n_observed
   )
   
@@ -61,26 +61,26 @@ nesteddata <- function(df, levels) {
 
 new_nesteddata <- function(sos_matrix,
                            group_sums,
-                           n_levels,
-                           levels,
-                           parents,
                            n_factors,
+                           factors,
+                           parents,
+                           n_levels,
                            n_observed) {
 
-  stopifnot(is.integer(n_levels))  
-  stopifnot(is.character(levels))
+  stopifnot(is.integer(n_factors))  
+  stopifnot(is.character(factors))
   stopifnot(is.list(parents) && all(sapply(parents, is.character)))
-  stopifnot(is.integer(n_factors))
+  stopifnot(is.integer(n_levels))
   stopifnot(is.list(n_observed) && all(sapply(n_observed, is.integer)))
   
   structure(
     list(sos_matrix = sos_matrix,
          group_sums = group_sums),
     class      = "nesteddata",
-    n_levels   = n_levels,
-    levels     = levels,
+    n_factors   = n_factors,
+    factors     = factors,
     parents    = parents,
-    n_factors  = n_factors,
+    n_levels  = n_levels,
     n_observed = n_observed
   )
  
--- tests/testthat/test-nesteddata.R
+++ tests/testthat/test-nesteddata.R
@@ -1,16 +1,20 @@
 set.seed(1234)
 
 df <- data.frame(
-  sire = c(1, 1, 1, 2, 2, 3, 3, 3, 3),
-  dam  = c(1, 1, 2, 3, 4, 5, 6, 6, 7),
-  ind  = 1:9,
-  v1   = rnorm(9),
-  v2   = rnorm(9),
-  v3   = rnorm(9)
+  sire = paste0("s", c(1, 1, 1, 2, 2, 3, 3, 3, 3)),
+  dam  = paste0("d", c(1, 1, 2, 3, 4, 5, 6, 6, 7)),
+  ind  = paste0("i", 1:9),
+  t1   = rnorm(9),
+  t2   = rnorm(9),
+  t3   = rnorm(9)
 )
 
-data <- nesteddata(df, levels = c("ind", "dam", "sire"))
+data <- nesteddata(df, factors = c("ind", "dam", "sire"))
 
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("Factor structure inferred correctly", {
+  expect_equal(attr(data, "n_factors"), 3) # 2-way model
+  expect_mapequal(attr(data, "n_observed")$dam,
+                  c(s1 = 2L, s2 = 2L, s3 = 3L))
+  expect_mapequal(attr(data, "parents")$dam,
+                  c(d1 = "s1", d2 = "s1", d3 = "s2", d4 = "s2", d5 = "s3" , d6 = "s3", d7 = "s3"))
 })
